0c63a3a16eb7dc0c95f89adc7f86b7c8b8a1e562
Renick Büttner <renick@staffbase.com>
2024-03-10 16:15:16 +0100

    feat: start reworking the MatomoReports class

--- classes/MatomoReports.php
+++ classes/MatomoReports.php
@@ -1,16 +1,32 @@
 <?php namespace Renick\Matomo\Classes;
 
+use Str;
+
 class MatomoReports
 {
     protected string $remote_url;
     protected string $auth_token;
     protected string $site_id;
+    protected int $limit;
+    protected string $period;
 
     public function __construct(string $url, string $token, int $siteId)
     {
-        $this->remote_url = $url;
         $this->auth_token = $token;
         $this->site_id = $siteId;
+        $this->remote_url = Str::endsWith('/', $url) ? $url : "{$url}/";
+        $this->limit = intval(env('MATOMO_REPORTS_LIMIT', 5));
+        $this->period = env('MATOMO_REPORTS_PERIOD', 'month');
+    }
+
+    protected function getRemoteURL(string $method): string
+    {
+        $url = "{$this->remote_url}";
+        $url .= "?module=API&method={$method}";
+        $url .= "&idSite={$this->site_id}&period={$this->period}&date=today";
+        $url .= "&format=JSON&filter_limit=$this->limit";
+        $url .= "&token_auth={$this->auth_token}";
+        return $url;
     }
 
     public function getVisitsSummary(): string
@@ -25,6 +41,12 @@ class MatomoReports
 
     public function getScreenSizeSummary(): string
     {
+        return "";
+    }
+
+    public function getCountriesSummery(): string
+    {
+        $url = $this->getRemoteURL('UserCountry.getCountry');
         return "";
     }
 
