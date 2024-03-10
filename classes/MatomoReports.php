a45b7e29bff470e80b7bf90148c9e242d969f774
Renick Büttner <renick@staffbase.com>
2024-03-10 16:10:43 +0100

    feat: remove dependencies

--- classes/MatomoReports.php
+++ classes/MatomoReports.php
@@ -1,72 +1,31 @@
 <?php namespace Renick\Matomo\Classes;
 
-use MatomoTracker;
-use VisualAppeal\Matomo;
-
 class MatomoReports
 {
-    private $api;
+    protected string $remote_url;
+    protected string $auth_token;
+    protected string $site_id;
 
     public function __construct(string $url, string $token, int $siteId)
     {
-        $this->api = new Matomo($url, $token, strval($siteId));
+        $this->remote_url = $url;
+        $this->auth_token = $token;
+        $this->site_id = $siteId;
     }
 
     public function getVisitsSummary(): string
     {
-        try {
-            $this->api->setPeriod(Matomo::PERIOD_DAY);
-            $this->api->setDate('previous30');
-
-            $img = base64_encode(
-                $this->api->getImageGraph(
-                    "VisitsSummary",
-                    "get",
-                    Matomo::GRAPH_EVOLUTION
-                )
-            );
-            return "data:image/png;base64,{$img}";
-        } catch (\Exception $e) {
-            return "";
-        }
+        return "";
     }
 
     public function getBrowsersSummary(): string
     {
-        try {
-            $this->api->setPeriod(Matomo::PERIOD_MONTH);
-            $this->api->setDate(Matomo::DATE_TODAY);
-
-            $img = base64_encode(
-                $this->api->getImageGraph(
-                    "DevicesDetection",
-                    "getBrowsers",
-                    "horizontalBar"
-                )
-            );
-            return "data:image/png;base64,{$img}";
-        } catch (\Exception $e) {
-            return "";
-        }
+        return "";
     }
 
     public function getScreenSizeSummary(): string
     {
-        try {
-            $this->api->setPeriod(Matomo::PERIOD_MONTH);
-            $this->api->setDate(Matomo::DATE_TODAY);
-
-            $img = base64_encode(
-                $this->api->getImageGraph(
-                    "Resolution",
-                    "getResolution",
-                    "horizontalBar"
-                )
-            );
-            return "data:image/png;base64,{$img}";
-        } catch (\Exception $e) {
-            return "";
-        }
+        return "";
     }
 
 }
